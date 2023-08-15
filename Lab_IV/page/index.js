a002db866506b9cbaae001dcf3c49d8dc0446593
jenibcunha <101344814+jenibcunha@users.noreply.github.com>
2023-08-15 11:00:38 -0300

    Add files via upload

---
+++ Lab_IV/page/index.js
@@ -0,0 +1,14 @@
+function validacao() {
+
+    let login = document.querySelector("#email").value;
+    let senha = document.querySelector("#senha").value;
+
+
+    if (login == "admin" && senha == "admin") {
+        alert("Sucesso")
+        location.href = "../paginas.home/home.html"
+    } else {
+        alert("Usuário ou senha incorretos!")
+    }
+
+}
